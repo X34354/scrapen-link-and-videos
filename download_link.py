66023eecf479ececaddbbc2bdc8c202ad5a15e6f
Species <francisco1997@ciencias.unam.mx>
2023-07-18 20:11:20 -0600

    upload

--- download_link.py
+++ download_link.py
@@ -2,8 +2,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
